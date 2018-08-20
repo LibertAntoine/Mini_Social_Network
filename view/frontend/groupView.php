3d4729a55565b4e494d0b2dee35b09337e62d11a
Liberta <antoine.libert@outlook.com>
2018-08-20 16:22:59 +0200

    add a GroupView page

--- view/frontend/groupView.php
+++ view/frontend/groupView.php
@@ -6,17 +6,43 @@ ob_start(); ?>
 
   	<div class="row">
         <div class="col-lg-8 col-md-7">
-            <?php foreach ($articles as $data) { ?>
-                <div class="articleBox jumbotron">
-                    <a href="index.php?action=article&amp;id=<?= $data->getId() ?>"><h3><?= htmlspecialchars($data->getTitle()) ?></h3></a>
-                    <em class="creationDate">ajouté le <?= $data->getCreationDate() ?></em>
-                    <p>
-                        <?= nl2br($data->getContent()) ?>
-                    </p>
-                </div>
-            <?php } ?>
+            <?php if ($posts !== 'none') { 
+                foreach ($posts as $data) { ?>
+                    <div class="postBox jumbotron">
+                        <h3><?= htmlspecialchars($data->getTitle()) ?></h3><br/>
+                        <em class="creationDate">ajouté le <?= $data->getCreationDate() ?></em><br/>
+                        <p><?= nl2br($data->getContent()) ?></p>
+                    </div>
+                    <?php if ($comments !== 'none') {
+                        foreach ($comments[$data->getId()] as $comment) { ?>
+                            <div class="postBox jumbotron">
+                                <em class="creationDate">ajouté le <?= $comment->getCreationDate() ?></em><br/>
+                                <p><?= nl2br($comment->getContent()) ?></p>
+                            </div>
+                        <?php } 
+                    } else { ?>
+                        <p>Le post ne compte aucun commentaire. Lancez-vous !</p>  
+                    <?php }
+                }
+            } else { ?>
+               <p>Le groupe ne contient encore aucun post. Lancez-vous !</p>  
+            <?php } ?>                     
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-lg-8 col-md-7">
+            <div class="jumbotron">
+                <h3>Ajouter un post au groupe</h3>
+                <form action="index.php?action=addPost" method="post">
+                    <label for="title">Titre :</label><br />
+                    <input type="text" id="title" name="title" />
+                    <label for="content">Contenu :</label><br />
+                    <textarea id="content" name="content"></textarea>
+                </form>
+            </div>
+        </div>
+    </div>        
 
 <?php $content = ob_get_clean(); ?>
 
